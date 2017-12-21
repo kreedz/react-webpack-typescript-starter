ea240eb05e40531c1fff7a0679bf2a86bdb47870
kreedz <kreedz@users.noreply.github.com>
2017-12-21 17:02:11 +0500

    webpack:  - replace 2 indent with 4  - use webpack 3 features

--- configs/webpack/common.js
+++ configs/webpack/common.js
@@ -6,56 +6,88 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const srcPath = resolve(__dirname, '../../src');
 
 module.exports = {
-  resolve: {
-    extensions: ['.ts', '.tsx', '.js', '.jsx'],
-  },
-  context: srcPath,
-  module: {
+    resolve: {
+        extensions: ['.ts', '.tsx', '.js', '.jsx'],
+    },
+    context: srcPath,
+    module: {
     rules: [
-      {
-          test: /\.tsx?$/,
-          use: [
-              {
-                  loader: 'babel-loader',
-                  options: {
-                      plugins: ['transform-runtime']
-                  }
+        {
+            test: /\.tsx?$/,
+            use: [
+                {
+                    loader: 'babel-loader',
+                    options: {
+                        plugins: ['transform-runtime']
+                    }
+                },
+                {
+                    loader: 'awesome-typescript-loader',
+                }
+            ],
+        },
+        {
+            test: /\.css$/,
+            use: [
+                'style-loader',
+                {
+                    loader: 'css-loader',
+                    options: {
+                        importLoaders: 1
+                    }
+                },
+                'postcss-loader',
+            ],
+        },
+        {
+            test: /\.scss$/,
+            use: [
+                'style-loader',
+                {
+                    loader: 'css-loader',
+                    options: {
+                        importLoaders: 2
+                    }
               },
-              {
-                  loader: 'awesome-typescript-loader',
-              }
-          ],
-      },
-      {
-        test: /\.css$/,
-        use: ['style-loader', { loader: 'css-loader', options: { importLoaders: 1 } }, 'postcss-loader',],
-      },
-      {
-        test: /\.scss$/,
-        loaders: [
-          'style-loader',
-          { loader: 'css-loader', options: { importLoaders: 1 } },
-          'postcss-loader',
-          'sass-loader',
-        ],
-      },
-      {
-        test: /\.(jpe?g|png|gif|svg)$/i,
-        loaders: [
-          'file-loader?hash=sha512&digest=hex&name=img/[hash].[ext]',
-          'image-webpack-loader?bypassOnDebug&optipng.optimizationLevel=7&gifsicle.interlaced=false',
-        ],
-      },
+              'postcss-loader',
+              'sass-loader',
+            ],
+        },
+        {
+            test: /\.(jpe?g|png|gif|svg)$/i,
+            use: [
+                {
+                    loader: 'file-loader',
+                    options: {
+                        hash: 'sha512',
+                        digest: 'hex',
+                        name: 'img/[hash].[ext]'
+                    }
+                },
+               {
+                   loader: 'image-webpack-loader',
+                   options: {
+                       bypassOnDebug: true,
+                       optipng: {
+                           optimizationLevel: 7,
+                       },
+                       gifsicle: {
+                           interlaced: false
+                       }
+                   }
+               }
+            ],
+        },
     ],
-  },
-  devtool: 'inline-source-map',
-  plugins: [
-    new CheckerPlugin(),
-    new StyleLintPlugin(),
-    new HtmlWebpackPlugin({
-        template: srcPath + '/index.html',
-        filename: 'index.html',
-        inject: 'body'
-    }),
-  ]
+    },
+    devtool: 'inline-source-map',
+    plugins: [
+        new CheckerPlugin(),
+        new StyleLintPlugin(),
+        new HtmlWebpackPlugin({
+            template: srcPath + '/index.html',
+            filename: 'index.html',
+            inject: 'body'
+        }),
+    ]
 };
--- configs/webpack/prod.js
+++ configs/webpack/prod.js
@@ -5,12 +5,12 @@ const {resolve} = require('path');
 const commonConfig = require('./common');
 
 module.exports = merge(commonConfig, {
-  entry: './index.tsx',
-  output: {
-    filename: 'js/bundle.[hash].min.js',
-    path: resolve(__dirname, '../../dist'),
-    publicPath: '/',
-  },
-  devtool: 'source-map',
-  plugins: [],
+    entry: './index.tsx',
+    output: {
+        filename: 'js/bundle.[hash].min.js',
+        path: resolve(__dirname, '../../dist'),
+        publicPath: '/',
+    },
+    devtool: 'source-map',
+    plugins: [],
 });
